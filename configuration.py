90441a3f2a0d6239b5bf7c13501f1a7fa88a359c
Fabian Affolter <mail@fabian-affolter.ch>
2017-04-06 20:43:14 +0200

    Add configuration file

---
+++ configuration.py
@@ -0,0 +1,15 @@
+# Device
+DEVICE_ID = ''
+
+# Wifi
+DEFAULT_WIFI_SSID = ''
+DEFAULT_WIFI_PASSWORD = ''
+
+CONFIG_WIFI_SSID = 'micropython-config'
+CONFIG_WIFI_PASSWORD = 'micropythoN'
+
+# MQTT
+MQTT_BROKER = '192.168.0.32'
+MQTT_PORT = 1883
+MQTT_USERNAME = ''
+MQTT_PASSWORD = '' 
